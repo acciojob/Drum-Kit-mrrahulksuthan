e796c29dd9e6e8f172e0d6c57ed3cb9ff6287f6b
mrrahulksuthan <86397472+mrrahulksuthan@users.noreply.github.com>
2023-06-10 13:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+window.addEventListener('keydown', function(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+  if (!audio) return; 
+
+  audio.currentTime = 0; 
+  audio.play(); 
+
+  key.classList.add('active'); 
+});
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('active');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(function(key) {
+  key.addEventListener('transitionend', removeTransition);
+});
