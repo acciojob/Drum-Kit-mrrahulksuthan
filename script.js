57d739b7e2178746b3d9ff45d928f5b83bbee8bf
mrrahulksuthan <86397472+mrrahulksuthan@users.noreply.github.com>
2023-06-10 13:24:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 //your JS code here. If required.
+
+
 const keySounds = {
-  65: 'C:\Users\rahul\Downloads\thunder-25689.mp3', 
-  83: 'sounds/sound2.mp3', 
-  68: 'sounds/sound3.mp3', 
-  70: 'sounds/sound4.mp3', 
-  71: 'sounds/sound5.mp3', 
-  72: 'sounds/sound6.mp3', 
-  74: 'sounds/sound7.mp3', 
-  75: 'sounds/sound8.mp3', 
-  76: 'sounds/sound9.mp3'  
+  65: 'C:\Users\rahul\Downloads\thunder-25689.mp3', // A
+  83: 'sounds/sound2.mp3', // S
+  68: 'sounds/sound3.mp3', // D
+  70: 'sounds/sound4.mp3', // F
+  71: 'sounds/sound5.mp3', // G
+  72: 'sounds/sound6.mp3', // H
+  74: 'sounds/sound7.mp3', // J
+  75: 'sounds/sound8.mp3', // K
+  76: 'sounds/sound9.mp3'  // L
 };
 
-window.addEventListener('keydown', function(event) {
-  const audioPath = keySounds[event.keyCode];
-  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+// Create an audio context
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
-  if (!audioPath || !key) 
+// Load sounds into audio buffers
+const audioBuffers = {};
+const loadSounds = async () => {
+  const keys = Object.keys(keySounds);
+  for (let i = 0; i < keys.length; i++) {
+    const keyCode = keys[i];
+    const soundPath = keySounds[keyCode];
 
-  const audio = new Audio(audioPath); 
-  audio.currentTime = 0; 
-  audio.play(); 
+    const response = await fetch(soundPath);
+    const arrayBuffer = await response.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+
+    audioBuffers[keyCode] = audioBuffer;
+  }
+};
+loadSounds();
+
+// Play sound from audio buffer
+const playSound = keyCode => {
+  const audioBuffer = audioBuffers[keyCode];
+
+  if (!audioBuffer) return;
+
+  const source = audioContext.createBufferSource();
+  source.buffer = audioBuffer;
+  source.connect(audioContext.destination);
+  source.start(0);
+};
+
+window.addEventListener('keydown', event => {
+  const keyCode = event.keyCode;
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+
+  if (!key) return; 
+
+  playSound(keyCode);
 
   key.classList.add('active'); 
 });
@@ -30,6 +62,7 @@ function removeTransition(event) {
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(function(key) {
+keys.forEach(key => {
   key.addEventListener('transitionend', removeTransition);
 });
+
