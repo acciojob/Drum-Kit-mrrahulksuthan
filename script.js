dfab8e8d7b82aba2febe5afd5f0610b1df00ae42
mrrahulksuthan <86397472+mrrahulksuthan@users.noreply.github.com>
2023-06-10 13:30:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,27 +8,27 @@ function makeSound(data-key){
             var audio1=new Audio("C:\Users\rahul\Downloads\thunder-25689.mp3");
             audio1.play();
             break;
-       case "a":
+       case "83":
             var audio2=new Audio("./sounds/kick-bass.mp3");
             audio2.play();
             break;
-       case "s":
+       case "68":
             var audio3=new Audio("./sounds/snare.mp3");
             audio3.play();
             break;
-       case "d":
+       case "70":
             var audio4=new Audio("./sounds/tom-1.mp3");
             audio4.play();
             break;
-        case "j":
+        case "71":
             var audio5=new Audio("./sounds/tom-2.mp3");
             audio5.play();
             break;
-        case "k":
+        case "72":
             var audio6=new Audio("./sounds/tom-3.mp3");
             audio6.play();
             break;
-        case "l":
+        case "74":
             var audio7=new Audio("./sounds/tom-4.mp3");
             audio7.play();
             break;    
